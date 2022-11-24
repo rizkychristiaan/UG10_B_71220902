bb7ee3f952b3855db245c2e822d073d44a1bbdc4
rizkychristiaan <117886340+rizkychristiaan@users.noreply.github.com>
2022-11-24 13:17:36 +0700

    Add files via upload

---
+++ soal 1.py
@@ -0,0 +1,24 @@
+print ("~~~~~~~~~~~~~~~ /('v')\ ~~~~~~~~~~~~~~~") 
+print ("PROGRAM PEGHITUNGAN VOLUME BANGUN RUANG") 
+print ("~~~~~~~~~~~~~~~ \('v')/ ~~~~~~~~~~~~~~~")
+print ("")
+print ("pilihlah salah satu bagun ruang  yang ingin dihitung volumenya: ")
+print ("1. limas")
+print ("2. bola")
+print ("3. prisma")
+print ("4. kerucut")
+print ("")
+
+pilihan = int(input("masukan pilihan anda"))
+if pilihan == 1:
+    alaslimas = int(input("masukan panjang sisi alas limas:"))
+    tinggilimas = int(input("masukan tinggi limas: "))
+    print("volume limas tersebut adalah ", alaslimas * alaslimas *)
+    elif pilihan == 3:
+        jaribola = int(input("masukan panjang jari-jari bola: "))
+        print ("volume bola tersebut adalah", 4/3*3.14*jaribola*jaribola*jaribola")
+
+
+
+
+
